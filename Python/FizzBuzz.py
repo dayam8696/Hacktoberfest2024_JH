1fffea0442837d9c8050561299f1f3d2d473c152
Md Sahil <113628373+sahil078@users.noreply.github.com>
2024-10-04 20:21:11 +0530

    Create FizzBuzz.py

---
+++ Python/FizzBuzz.py
@@ -0,0 +1,29 @@
+def fizz_buzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Call the function
+fizz_buzz() 
+
+
+Output: 
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+...
+
+
